20f7ef4117f636f144825fb8b1961ef5135a65ed
unknown <dlaehdwns2020@naver.com>
2023-02-04 13:24:25 +0900

    my first commit

---
+++ ScannerClass.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+// 자바 입출력에 대한 Scanner 코드입니다.
+public class ScannerClass {
+
+    public static void main(String[] args) {
+        System.out.println("아무 문자열이나 입력해주세요");
+        Scanner sc = new Scanner(System.in); // 입력 받을때는  out이 아니라 in
+        String input = sc.nextLine();
+        System.out.println("입력하신 문자열은 아래에 있습니다.");
+        System.out.println(input);
+    }
+}
---
+++ Valieables.java
@@ -0,0 +1,6 @@
+// java 변수 기초에 대한 클래스입니다.
+public class Valieables {
+    public static void main(String[] args) {
+
+    }
+}
