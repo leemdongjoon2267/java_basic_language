10996932aa424e471f3a830a7ea084f76b34804f
unknown <dlaehdwns2020@naver.com>
2023-02-12 13:13:22 +0900

    days4 class

--- CodingTest.java
+++ CodingTest.java
@@ -1,13 +1,46 @@
+import java.util.Scanner;
+
 public class CodingTest {
     public static void main(String[] args) {
-        int num1 = 1;
-        int num2 = -1;
-        int answer;
-        if(num1==num2){
-            answer = 1;
-        }else{
-            answer = -1;
-        }
+//        int a = 32;
+//        int answer = 0;
+//        answer += a%10; //1
+//        answer += a/10; //3
+
+//        int b = 321;
+//        answer += b%10; // answer에 더할 값 1
+//        b/=10; // b:32
+//        answer += b%10; // answer에 더할 값 2
+//        b/=10; // b:3
+//        answer += b%10; // answer에 더할 값 3
+//
+//        int c = 4321;
+//        answer += c%10; // answer에 더할 값 1
+//        c/=10; // c:432
+//        answer += c%10; // answer에 더할 값 2
+//        c/=10; // c:43
+//        answer += c%10; // answer에 더할 값 3
+//        c/=10; // c:4
+//        answer += c%10; // answer에 더할 값 4
+//        System.out.println((a/10)+(a%10));
+//        System.out.println(((b/10)/10)+((b/10)%10)+((b%10))%10);
+//        System.out.println((((c/10)/10)/10)+(((c/10)/10)%10)+((((c/10)%10)%10)%10)+(((((c%10)%10)%10)%10)%10));
+
+//        int n = 1;
+//        int answer = 0;
+//
+//        while(n > 0){
+//            answer += n%10;
+//            n/=10;
+//        }
+//        System.out.println(answer);
+
+
 
     }
 }
+
+
+
+
+
--- LoopStatement.java
+++ LoopStatement.java
@@ -29,14 +29,124 @@ public class LoopStatement {
 //            System.out.println(num*i);
 //        }
 
-        System.out.println("구구단을 입력하세요");
-        Scanner sc = new Scanner(System.in); // 입력 받을때는  out이 아니라 in
-        int input = sc.nextInt();
+//        System.out.println("구구단을 입력하세요");
+//        Scanner sc = new Scanner(System.in); // 입력 받을때는  out이 아니라 in
+//        int input = sc.nextInt();
+//
+//        int num = 1;
+//        while(num<10){
+//            System.out.println(input+"X"+num+"="+input*num);
+//            num = num + 1;
+//        }
 
-        int num = 1;
-        while(num<10){
-            System.out.println(input+"X"+num+"="+input*num);
-            num = num + 1;
+//        int i = 1;
+//        int j = 9;
+//        while(i<10){
+//            System.out.println(i+"X"+j+"="+i*j);
+//            i++;
+//            j--;
+//        }
+
+        //        약수의 개수 구하기
+//
+//        System.out.println("아무 숫자를 입력해주세요");
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//       // int n = 100;
+//        int count = 0;
+//        int x = 1;
+//
+//        while(x <= n){
+//            if(n%x==0){
+//                count++;
+//            }
+//            x++;
+//        }
+//        System.out.println(count);
+
+//       사칙연산 프로그램 : while, switch
+//        스캐너를 통해서 +-*/를 하나 입력
+//        두개의 숫자 입력
+//        while문은 무한 반복
+//        while(true){
+//            System.out.println("원하는 사칙연산은 입력하세요");
+//            Scanner sc = new Scanner(System.in);
+//            String str = sc.nextLine();
+//            System.out.println("첫번째 숫자를 입력하세요");
+//            double n = sc.nextDouble();
+//            System.out.println("두번째 숫자를 입력하세요");
+//            double x = sc.nextDouble();
+//
+//            double result = 0;
+//
+//            switch(str){
+//                case "+" :
+//                    result = (n+x);
+//                    break;
+//                case "-" :
+//                    result = (n-x);
+//                    break;
+//                case "*":
+//                    result = (n*x);
+//                    break;
+//                case "/" :
+//                    result = (n/x);
+//                    break;
+//            }
+//            System.out.println("결과는 :"+result);
+//        }
+//        변수의 유효범위는 {}까지만 유효하고 하위 depth에서는 상위 depth값을 알지만
+//        싱으; depth에서는 하위 depth에서 선언된 값을 알지 못한다
+//        int a = 0;
+//        while(a>100){
+//            System.out.println("hello"+a);
+//            a++;
+//        }
+//        System.out.println(a);
+//        for(int a=100; a>0; a--){
+//            System.out.println("hello"+a);
+//        }
+//        System.out.println(a);
+
+
+
+//        배열
+//        int n1 = 10;
+//        int n2 = 20;
+//        int n3 = 30;
+//        System.out.println(n1);
+//        int[] arr1 = {10,20,30,40,50};
+//        for(int i=0; i<arr1.length; i++){
+//            arr1[i] = arr1[i]+10;
+//            System.out.println(arr1[i]);
+//        }
+
+//        enhanced for문 : for each문
+//        for(int a : arr1){
+//            System.out.println(a);
+//        }
+
+//        String 타입의 배열로 "aaa", "bbb", "ccc", "ddd", "fff"를 담고  for each문으로 출력
+//        String[] arr2 = {"aaa","bbb","ccc","ddd","fff"};
+//        for(int i=0; i<arr2.length; i++){
+//            System.out.println(arr2[i]);
+//        }
+//
+//        for(String a : arr2){
+//            System.out.println(a);
+//        }
+
+//        2중(다중) 반복문
+//        int num = 2;
+//        for(int i=1; i<10; i++){
+//            System.out.println(num+"X"+i+"="+num*i);
+//        }
+
+        for(int i=2; i<10; i++){
+            System.out.println(i+"단입니다.");
+            for(int j=1; j<10; j++){
+                System.out.println(i+"X"+j+"="+i*j);
+            }
         }
     }
 }
